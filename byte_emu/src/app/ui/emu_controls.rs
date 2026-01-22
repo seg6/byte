64738c9f15cd0f032e332764f9e3e8becb059b42
seg6 <hi@seg6.space>
2026-01-22 01:48:26 +0100

    ​
    Signed-off-by: seg6 <hi@seg6.space>

---
+++ byte_emu/src/app/ui/emu_controls.rs
@@ -0,0 +1,14 @@
+use eframe::egui;
+use crate::app::ByteEmuApp;
+
+impl ByteEmuApp {
+    pub fn show_emu_controls(&mut self, ctx: &egui::Context) {
+        let mut open = self.state.is_emu_controls_open;
+        egui::Window::new("Emulator Controls")
+            .open(&mut open)
+            .show(ctx, |ui| {
+                ui.label("TODO");
+            });
+        self.state.is_emu_controls_open = open;
+    }
+}
---
+++ byte_emu/src/app/ui/menu_bar.rs
@@ -0,0 +1,69 @@
+use eframe::egui;
+use crate::app::{ByteEmuApp, FileProcesserMessage, State};
+
+impl ByteEmuApp {
+    pub fn show_menu_bar(&mut self, ctx: &egui::Context) {
+        egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
+            self.ui_menu_bar(ui);
+        });
+    }
+
+    fn ui_menu_bar(&mut self, ui: &mut egui::Ui) {
+        egui::MenuBar::new().ui(ui, |ui| {
+            self.ui_file_button(ui);
+            self.ui_tools_button(ui);
+
+            if ui.button("About").clicked() {
+                self.state.is_about_open = !self.state.is_about_open;
+            }
+        });
+    }
+
+    fn ui_file_button(&mut self, ui: &mut egui::Ui) {
+        ui.menu_button("File", |ui| {
+            use FileProcesserMessage::*;
+
+            if ui.button("Load binary program").clicked() {
+                self.file_processer
+                    .read(|name, data| BinaryFile((name, data)));
+                ui.close();
+            }
+            if ui.button("Load source file").clicked() {
+                self.file_processer
+                    .read(|name, data| SourceFile((name, data)));
+                ui.close();
+            }
+
+            ui.separator();
+
+            if ui.button("Reset GUI state").clicked() {
+                ui.ctx().memory_mut(|mem| *mem = Default::default());
+                ui.close();
+            }
+            if ui.button("Reset everything").clicked() {
+                self.state = State::default();
+                ui.ctx().memory_mut(|mem| *mem = Default::default());
+                ui.close();
+            }
+        });
+    }
+
+    fn ui_tools_button(&mut self, ui: &mut egui::Ui) {
+        ui.menu_button("Tools", |ui| {
+            if ui.button("Code Editor").clicked() {
+                self.state.is_code_editor_open = !self.state.is_code_editor_open;
+                ui.close();
+            }
+
+            if ui.button("Emulator Controls").clicked() {
+                self.state.is_emu_controls_open = !self.state.is_emu_controls_open;
+                ui.close();
+            }
+
+            if ui.button("Memory Monitor").clicked() {
+                self.state.is_memory_monitor_open = !self.state.is_memory_monitor_open;
+                ui.close();
+            }
+        });
+    }
+}
